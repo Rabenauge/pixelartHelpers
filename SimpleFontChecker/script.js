35bf6de18d453ab13f582c98f5d502cb427e3e58
Titus Grothaus <titus@rabenauge.com>
2023-02-10 15:49:22 +0100

    cleanup code a bit

--- SimpleFontChecker/script.js
+++ SimpleFontChecker/script.js
@@ -2,6 +2,8 @@
   * done by titus^rabenauge
   */
 
+//set up some simple classes to help with some readable code
+
 class calcTile {
     columns = 0;
     rows = 0;
@@ -55,40 +57,35 @@ class font {
     }
 }
 
+// start here for the vars
 
 const myImage = new Image();
 myImage.src = "font.png";
 const canvas= document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+//vars you might want to change
+
 canvas.width = 1024; //size x preview
 canvas.height = 1024; //size y preview
-
-const gridW = 16, //
-      gridH = 16,
-      ignoreChars = [0,3,4,5,6,7,10,15,28,30,32,59] //just the plain number of the char to ignore
+const gridW = 16, //x font width
+      gridH = 16, //y font heigth
+      ignoreChars = [0,3,4,5,6,7,10,15,28,30,32,59] //just the plain tile number of the chars 
+                    //to ignore, keep that array empty if you don´t want to skip any
       ;
-
-
-
 
 myFont = new font(myImage.width,myImage.height,gridW,gridH),ignoreChars;
 
-
 myImage.addEventListener('load', function(){
-
     let targetCalc = new calcTile();
     targetCalc.calcRowAndColumns(canvas.width,canvas.height,gridH,gridW);
-
     let posX = 0;
     let posY = 0;
-
-
-        for (let r = 0; r < targetCalc.rows; r++ ) {
-            for (let c = 0; c < targetCalc.columns; c++ ) {
-                rndTilePos = myFont.randomCharCoords(); 
-                ctx.drawImage(myImage, rndTilePos[1], rndTilePos[0] ,gridW,gridH,c*gridW,r*gridH,gridW,gridH);
-            }    
-        }
+    for (let r = 0; r < targetCalc.rows; r++ ) {
+        for (let c = 0; c < targetCalc.columns; c++ ) {
+            rndTilePos = myFont.randomCharCoords(); 
+            ctx.drawImage(myImage, rndTilePos[1], rndTilePos[0] ,gridW,gridH,c*gridW,r*gridH,gridW,gridH);
+        }    
+    }
 });
 
